ee599135d08cf7337139957bda105478fd79c215
筍包啦 <108303348+swenbaoQQ@users.noreply.github.com>
2022-11-21 22:39:23 +0800

    Update Lab10.v

--- Lab10/Lab10.v
+++ Lab10/Lab10.v
@@ -8,8 +8,9 @@ wire [1:0] state;
 wire [3:0] count;
 
 clk_div_7(clock, reset, clk7);
+clk_div_dot(clock, reset, clk_dot);
 count_down(clk7, reset, state, count);
 sevenDisplay(count, a, b, c, d, e, f, g);
-dot_diaplay(div_clk_dot, reset, dot_row, dot_col);
+dot_diaplay(state, clk_dot, reset, dot_row, dot_col);
 
 endmodule

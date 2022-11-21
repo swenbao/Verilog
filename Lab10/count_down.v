d0d44e57d069ecc036ce4c28e3880a5276ff89cf
筍包啦 <108303348+swenbaoQQ@users.noreply.github.com>
2022-11-21 22:40:18 +0800

    Update and rename count_down to count_down.v

--- Lab10/count_down.v
+++ Lab10/count_down.v
@@ -6,17 +6,24 @@ output reg [3:0] count;
 always@(posedge clock_div or negedge reset)
 begin
     if(reset == 0)
+	 begin
         count = 4'b1111;
         state = 0;
+	 end
     else
     begin
         count = count - 1;
         if(count == 0)
         begin
             case(state)
-                2'b00: state = state + 1; count = 4'b0101;
-                2'b01: state = state + 1; count = 4'b1010;
-                default: state = 0; count = 4'b1111;
+                2'b00: state = state + 1;
+                2'b01: state = state + 1;
+                default: state = 0;
+            endcase
+				case(state)
+                2'b00: count = 4'b1111;
+                2'b01: count = 4'b0101;
+                default: count = 4'b1010;
             endcase
         end
     end

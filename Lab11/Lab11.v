bf1cab4f76f7035a37f707589b1a8627b4f90321
筍包啦 <108303348+swenbaoQQ@users.noreply.github.com>
2022-12-06 16:41:24 +0800

    Update Lab11.v

--- Lab11/Lab11.v
+++ Lab11/Lab11.v
@@ -1,15 +1,16 @@
-module Lab11()
+module Lab11(clock, reset, keypadCol, keypadRow, dot_row, dot_col);
 
     input clock, reset;
     input [3:0] keypadCol;
-    output reg [7:0] dot_row, dot_col;    
+    output [7:0] dot_row, dot_col;
+	 output [3:0] keypadRow;
 
     wire div_clk_100, div_clk_10000;
     wire [3:0] in;
 
     clk_div_100(clock, reset, div_clk_100);
     clk_div_10000(clock, reset, div_clk_10000);
-    checkkeypad(div_clk_100, reset, keypadCol, in);
+    checkkeypad(div_clk_100, reset, keypadCol, keypadRow, in);
     dot_diaplay(div_clk_10000, reset, in, dot_row, dot_col);
     
-endmodule
+endmodule 
